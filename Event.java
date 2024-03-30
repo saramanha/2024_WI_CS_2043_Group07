2269e213a2d9dabc1475838a42cd72ff38cf9c50
kamenshchykovaM <134241743+kamenshchykovaM@users.noreply.github.com>
2024-03-30 10:20:49 -0300

    Update Event.java

--- Event.java
+++ Event.java
@@ -1,31 +1,26 @@
-/**
- * This class allows the gym staff to add and remove a fitness events, set a price, capacity, inventory needed etc.
- * @author Mariia Kamenshchykova
-*/
 
-//using if statement in setter and call it from constructor
+import java.time.LocalDate;
 
-import java.util.ArrayList; // import the ArrayList class
 
 
 public class Event {
 
 	private String name;
 	private int capacity;
-	private String inventory;
+	private Inventory inventory;
 	private LocalDate date;
 	private double price;
-	private Instructor instructor;
+	private GymWorker gymWorker;
 	
 
-	private Event(String name, int capacity, Inventory inventory, double price, LocalDate date, Instructor instructor){
+	public Event(String name, int capacity, Inventory inventory, double price, LocalDate date, GymWorker gymWorker){
 
 			this.name = name;
 			this.capacity = capacity;
 			this.inventory = inventory;
 			this.price = price;
 			this.date = date;
-			this.instructor = instructor;
+			this.gymWorker = gymWorker;
 		}
 	
 
@@ -46,45 +41,48 @@ public class Event {
 	public LocalDate getDate(){
 		return date;
 	}
-	public Instructor getInstructor(){
-		return Instructor;
+	public String getGymWorkerName(){
+		return gymWorker.getName();
 	}
 
+	public String getInventory(){
+		return inventory.getType();
+	}
 
 // setters
 
 	public void setName(String newName){
-		name = newName:
+		name = newName;
 	}
 
 	public void setPrice(double newPrice){
-		price = newPrice:
+		price = newPrice;
 	}
 
 	public void setCapacity(int newCapacity){
 		capacity = newCapacity;
 	}
 
-	public void setInventory(Inventory newInventory){
-		//again check if the inventory is available
-		inventory = newInventory;
+	public void setInventory(Inventory inventoryIn){
+		
+		inventory = new Inventory(inventoryIn.getType(), inventoryIn.getAmount());
 	}
 
 	public void setDate(LocalDate newDate){
 		//check if available
-		date = newDate:
+		date = newDate;
 	}
 
-	public void setInstructor(Instructor newInstructor){
-		instructor = newInstructor:
+	public void setWorker(GymWorker newGymWorker){
+		gymWorker = newGymWorker;
 	}
 
 
 // overriding toString()
 
-public toString(){
+public String toString(){
 
-	return "Event: " + name + " Max. capacity: " + capacity + " The price: $" + price + " Date: " + date + " Instructor name: " + instructor + ".\n" ;
+	return "Event: " + name + " Max. capacity: " + capacity + " The price: $" + price + " Date: " + date + " Instructor name: " + gymWorker.getName() + ".\n" ;
 	}
 
 }
