3f797e4b203b69b6e622352d9e96e62040d3b032
tmngr <61593534+tmngr@users.noreply.github.com>
2024-04-04 12:19:01 -0300

    Update Member.java

--- Member.java
+++ Member.java
@@ -3,14 +3,13 @@ public class Member{
     private String name;
     private String email;
     private String phone;
-    private final int id;
-    private static int nextId = 1000;
+    private int id;
 
-    public Member(String nameIn, String emailIn, String phoneIn){
+    public Member(String nameIn, String emailIn, String phoneIn, int idIn){
         name = nameIn;
         email = emailIn;
         phone = phoneIn;
-        id = nextId++;
+        id = idIn;
     }
 
     public String getName(){ return name; }
@@ -23,7 +22,7 @@ public class Member{
     public void setPhone(String newPhone){ phone = newPhone; }
 
     public void displayInfo(){
-        System.out.println("Member Id: " + String.format("%0.4d", id) + ", Name: " + name +
+        System.out.println("Member Id: " + String.valueOf(id) + ", Name: " + name +
                            ", Email: " + email + ", Phone: " + phone);
     }
 }
